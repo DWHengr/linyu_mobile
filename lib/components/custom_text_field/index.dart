535f6de43f05192e8301a660530dd3711df9a413
Heath <duweihengmr@163.com>
2024-10-29 21:01:59 +0800

    login page

---
+++ lib/components/custom_text_field/index.dart
@@ -0,0 +1,56 @@
+// custom_text_field.dart
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatelessWidget {
+  final String labelText;
+  final TextEditingController controller;
+  final bool obscureText;
+  final String hintText;
+
+  const CustomTextField({
+    super.key,
+    required this.labelText,
+    required this.controller,
+    this.hintText = '请输入内容',
+    this.obscureText = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start, // 标签左对齐
+      children: [
+        Text(
+          labelText,
+          style:
+              const TextStyle(color: Color(0xFF1F1F1F), fontSize: 14.0), // 标签样式
+        ),
+        const SizedBox(height: 5.0), // 标签和输入框之间的间距
+        Container(
+          decoration: BoxDecoration(
+            color: const Color(0xFFEDF2F9), // 设置背景颜色为 #EDF2F9
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+          child: TextField(
+            controller: controller,
+            obscureText: obscureText,
+            decoration: InputDecoration(
+              hintText: hintText,
+              hintStyle: const TextStyle(color: Colors.grey, fontSize: 14.0),
+              filled: true,
+              // 填充背景
+              fillColor: const Color(0xFFEDF2F9),
+              // 填充背景颜色
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10.0),
+                borderSide: BorderSide.none, // 去除边框
+              ),
+              contentPadding:
+                  const EdgeInsets.symmetric(vertical: 0.0, horizontal: 8.0),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/pages/login/index.dart
@@ -0,0 +1,137 @@
+import 'package:flutter/material.dart';
+
+import '../../components/custom_text_field/index.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _usernameController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  void _login() {
+    String username = _usernameController.text;
+    String password = _passwordController.text;
+
+    if (username == "admin" && password == "123456") {
+      showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: Text("登录成功"),
+          content: Text("欢迎，$username!"),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: Text("确定"),
+            ),
+          ],
+        ),
+      );
+    } else {
+      showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: Text("登录失败"),
+          content: Text("用户名或密码错误，请重试"),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: Text("确定"),
+            ),
+          ],
+        ),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Color(0xFFBED7F6), Color(0xFFFFFFFF), Color(0xFFDFF4FF)],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: Padding(
+          padding:
+              EdgeInsets.only(bottom: MediaQuery.of(context).size.height * 0.2),
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                // 应用 Logo 和名称
+                Image.asset(
+                  'assets/images/logo.png', // 确保在pubspec.yaml中添加了logo图片路径
+                  height: screenWidth * 0.3,
+                  width: screenWidth * 0.3,
+                ),
+                const Text(
+                  "林语",
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.w900,
+                  ),
+                ),
+                const SizedBox(height: 32.0),
+                Container(
+                  padding: const EdgeInsets.symmetric(
+                      vertical: 30.0, horizontal: 20.0),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10.0),
+                    border: Border.all(
+                      color: const Color(0xFFF2F2F2), // 边框颜色
+                      width: 1.0, // 边框宽度
+                    ),
+                  ),
+                  child: Column(
+                    children: <Widget>[
+                      CustomTextField(
+                        labelText: "账号",
+                        controller: _usernameController,
+                      ),
+                      const SizedBox(height: 15.0),
+                      CustomTextField(
+                        labelText: "密码",
+                        controller: _passwordController,
+                        obscureText: true, // 密码输入框
+                      ),
+                      const SizedBox(height: 20.0),
+                      FractionallySizedBox(
+                        widthFactor: 0.8,
+                        child: ElevatedButton(
+                          onPressed: _login,
+                          style: ElevatedButton.styleFrom(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 10, vertical: 10),
+                            backgroundColor: const Color(0xFF4C9BFF),
+                            foregroundColor: Colors.white,
+                          ),
+                          child: const Text(
+                            "登  录",
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
