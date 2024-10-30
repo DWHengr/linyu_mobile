a13d029c549c98d2dbfa73c50378ef2172657ad0
Heath <duweihengmr@163.com>
2024-10-30 23:02:55 +0800

    user api

--- lib/pages/login/index.dart
+++ lib/pages/login/index.dart
@@ -1,13 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:linyu_mobile/api/user_api.dart';
-import 'package:encrypt/encrypt.dart' as encrypt;
-import 'package:linyu_mobile/pages/chat_list/index.dart';
-import 'package:pointycastle/asymmetric/api.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import '../navigation/index.dart';
-import '../../components/custom_text_field/index.dart';
 
-final _useApi = UserApi();
+import '../../components/custom_text_field/index.dart';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -18,64 +11,47 @@ class _LoginPageState extends State<LoginPage> {
   final _usernameController = TextEditingController();
   final _passwordController = TextEditingController();
 
-  void _showDialog(String content, [String title = '登录失败']) {
-    showDialog(
-      context: context,
-      builder: (context) => AlertDialog(
-        title: Text(title),
-        content: Text(content),
-        actions: [
-          TextButton(
-            onPressed: () => Navigator.pop(context),
-            child: Text("确定"),
-          ),
-        ],
-      ),
-    );
-  }
-
-  void _login() async {
+  void _login() {
     String username = _usernameController.text;
     String password = _passwordController.text;
-    if (username.isEmpty || password.isEmpty) {
-      _showDialog("用户名或密码不能为空~");
-      return;
-    }
-    final publicKeyResult = await _useApi.publicKey();
-    if (publicKeyResult['code'] != 0) {}
-    String key = publicKeyResult['data'];
-    final parsedKey = encrypt.RSAKeyParser().parse(key) as RSAPublicKey;
-    final encrypter = encrypt.Encrypter(encrypt.RSA(publicKey: parsedKey));
-    final encryptedPassword = encrypter.encrypt(password).base64;
-    final loginResult = await _useApi.login(username, encryptedPassword);
-    if (loginResult['code'] == 0) {
-      SharedPreferences prefs = await SharedPreferences.getInstance();
-      await prefs.setString('x-token', loginResult['data']['token']);
-      Navigator.of(context).pushAndRemoveUntil(
-        MaterialPageRoute(builder: (context) => CustomBottomNavigationBar()),
-        (route) => false,
+
+    if (username == "admin" && password == "123456") {
+      showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: Text("登录成功"),
+          content: Text("欢迎，$username!"),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: Text("确定"),
+            ),
+          ],
+        ),
       );
     } else {
-      _showDialog("用户名或密码错误，请重试尝试~");
+      showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: Text("登录失败"),
+          content: Text("用户名或密码错误，请重试"),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: Text("确定"),
+            ),
+          ],
+        ),
+      );
     }
-  }
-
-  @override
-  void dispose() {
-    _usernameController.dispose();
-    _passwordController.dispose();
-    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     final screenWidth = MediaQuery.of(context).size.width;
-    final screenHeight = MediaQuery.of(context).size.height;
-
     return Scaffold(
       resizeToAvoidBottomInset: false,
       body: Container(
-        height: screenHeight,
         decoration: const BoxDecoration(
           gradient: LinearGradient(
             colors: [Color(0xFFBED7F6), Color(0xFFFFFFFF), Color(0xFFDFF4FF)],
@@ -83,85 +59,75 @@ class _LoginPageState extends State<LoginPage> {
             end: Alignment.bottomRight,
           ),
         ),
-        child: SafeArea(
-          child: SingleChildScrollView(
-            child: Container(
-              height: screenHeight -
-                  MediaQuery.of(context).padding.top -
-                  MediaQuery.of(context).padding.bottom,
-              padding: const EdgeInsets.symmetric(horizontal: 16.0),
-              child: Column(
-                children: [
-                  const Spacer(flex: 1),
-                  // Logo部分
-                  Image.asset(
-                    'assets/images/logo.png',
-                    height: screenWidth * 0.25,
-                    width: screenWidth * 0.25,
+        child: Padding(
+          padding:
+              EdgeInsets.only(bottom: MediaQuery.of(context).size.height * 0.2),
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                // 应用 Logo 和名称
+                Image.asset(
+                  'assets/images/logo.png', // 确保在pubspec.yaml中添加了logo图片路径
+                  height: screenWidth * 0.3,
+                  width: screenWidth * 0.3,
+                ),
+                const Text(
+                  "林语",
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.w900,
                   ),
-                  const Text(
-                    "林语",
-                    style: TextStyle(
-                      fontSize: 30,
-                      fontWeight: FontWeight.w900,
+                ),
+                const SizedBox(height: 32.0),
+                Container(
+                  padding: const EdgeInsets.symmetric(
+                      vertical: 30.0, horizontal: 20.0),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10.0),
+                    border: Border.all(
+                      color: const Color(0xFFF2F2F2), // 边框颜色
+                      width: 1.0, // 边框宽度
                     ),
                   ),
-                  const SizedBox(height: 20.0),
-                  // 登录框部分
-                  Container(
-                    padding: const EdgeInsets.symmetric(
-                      vertical: 20.0,
-                      horizontal: 20.0,
-                    ),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(10.0),
-                      border: Border.all(
-                        color: const Color(0xFFF2F2F2),
-                        width: 1.0,
+                  child: Column(
+                    children: <Widget>[
+                      CustomTextField(
+                        labelText: "账号",
+                        controller: _usernameController,
                       ),
-                    ),
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: <Widget>[
-                        CustomTextField(
-                          labelText: "账号",
-                          controller: _usernameController,
-                        ),
-                        const SizedBox(height: 15.0),
-                        CustomTextField(
-                          labelText: "密码",
-                          controller: _passwordController,
-                          obscureText: true,
-                        ),
-                        const SizedBox(height: 20.0),
-                        FractionallySizedBox(
-                          widthFactor: 0.8,
-                          child: ElevatedButton(
-                            onPressed: _login,
-                            style: ElevatedButton.styleFrom(
-                              padding: const EdgeInsets.symmetric(
-                                horizontal: 5,
-                                vertical: 5,
-                              ),
-                              backgroundColor: const Color(0xFF4C9BFF),
-                              foregroundColor: Colors.white,
-                            ),
-                            child: const Text(
-                              "登  录",
-                              style: TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.bold,
-                              ),
+                      const SizedBox(height: 15.0),
+                      CustomTextField(
+                        labelText: "密码",
+                        controller: _passwordController,
+                        obscureText: true, // 密码输入框
+                      ),
+                      const SizedBox(height: 20.0),
+                      FractionallySizedBox(
+                        widthFactor: 0.8,
+                        child: ElevatedButton(
+                          onPressed: _login,
+                          style: ElevatedButton.styleFrom(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 10, vertical: 10),
+                            backgroundColor: const Color(0xFF4C9BFF),
+                            foregroundColor: Colors.white,
+                          ),
+                          child: const Text(
+                            "登  录",
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
                             ),
                           ),
-                        )
-                      ],
-                    ),
+                        ),
+                      )
+                    ],
                   ),
-                  const Spacer(flex: 3),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
         ),
