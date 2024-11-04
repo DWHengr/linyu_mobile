f614e7afb9bcd508003aeaec4163fba18d2247d2
Heath <duweihengmr@163.com>
2024-11-04 21:37:50 +0800

    modify style

--- lib/pages/login/index.dart
+++ lib/pages/login/index.dart
@@ -56,9 +56,12 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
+
     return Scaffold(
-      resizeToAvoidBottomInset: true,
+      resizeToAvoidBottomInset: false,
       body: Container(
+        height: screenHeight,
         decoration: const BoxDecoration(
           gradient: LinearGradient(
             colors: [Color(0xFFBED7F6), Color(0xFFFFFFFF), Color(0xFFDFF4FF)],
@@ -67,79 +70,84 @@ class _LoginPageState extends State<LoginPage> {
           ),
         ),
         child: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            child: Column(
-              children: [
-                const Spacer(flex: 1),
-                // Logo部分
-                Image.asset(
-                  'assets/images/logo.png',
-                  height: screenWidth * 0.25,
-                  width: screenWidth * 0.25,
-                ),
-                const Text(
-                  "林语",
-                  style: TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.w900,
+          child: SingleChildScrollView(
+            child: Container(
+              height: screenHeight -
+                  MediaQuery.of(context).padding.top -
+                  MediaQuery.of(context).padding.bottom,
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Column(
+                children: [
+                  const Spacer(flex: 1),
+                  // Logo部分
+                  Image.asset(
+                    'assets/images/logo.png',
+                    height: screenWidth * 0.25,
+                    width: screenWidth * 0.25,
                   ),
-                ),
-                const SizedBox(height: 20.0),
-                // 登录框部分
-                Container(
-                  padding: const EdgeInsets.symmetric(
-                    vertical: 20.0,
-                    horizontal: 20.0,
-                  ),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(10.0),
-                    border: Border.all(
-                      color: const Color(0xFFF2F2F2),
-                      width: 1.0,
+                  const Text(
+                    "林语",
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.w900,
                     ),
                   ),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: <Widget>[
-                      CustomTextField(
-                        labelText: "账号",
-                        controller: _usernameController,
+                  const SizedBox(height: 20.0),
+                  // 登录框部分
+                  Container(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 20.0,
+                      horizontal: 20.0,
+                    ),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10.0),
+                      border: Border.all(
+                        color: const Color(0xFFF2F2F2),
+                        width: 1.0,
                       ),
-                      const SizedBox(height: 15.0),
-                      CustomTextField(
-                        labelText: "密码",
-                        controller: _passwordController,
-                        obscureText: true,
-                      ),
-                      const SizedBox(height: 20.0),
-                      FractionallySizedBox(
-                        widthFactor: 0.8,
-                        child: ElevatedButton(
-                          onPressed: _login,
-                          style: ElevatedButton.styleFrom(
-                            padding: const EdgeInsets.symmetric(
-                              horizontal: 5,
-                              vertical: 5,
-                            ),
-                            backgroundColor: const Color(0xFF4C9BFF),
-                            foregroundColor: Colors.white,
-                          ),
-                          child: const Text(
-                            "登  录",
-                            style: TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
+                    ),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        CustomTextField(
+                          labelText: "账号",
+                          controller: _usernameController,
                         ),
-                      )
-                    ],
+                        const SizedBox(height: 15.0),
+                        CustomTextField(
+                          labelText: "密码",
+                          controller: _passwordController,
+                          obscureText: true,
+                        ),
+                        const SizedBox(height: 20.0),
+                        FractionallySizedBox(
+                          widthFactor: 0.8,
+                          child: ElevatedButton(
+                            onPressed: _login,
+                            style: ElevatedButton.styleFrom(
+                              padding: const EdgeInsets.symmetric(
+                                horizontal: 5,
+                                vertical: 5,
+                              ),
+                              backgroundColor: const Color(0xFF4C9BFF),
+                              foregroundColor: Colors.white,
+                            ),
+                            child: const Text(
+                              "登  录",
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
                   ),
-                ),
-                const Spacer(flex: 3),
-              ],
+                  const Spacer(flex: 3),
+                ],
+              ),
             ),
           ),
         ),
